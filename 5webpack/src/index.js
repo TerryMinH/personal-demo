6e79e93a59651f78b0a02c265139f6b0ca14ffbb
TerryMin <15896938825@163.com>
2021-03-12 16:49:42 +0800

    webpack

--- 5webpack/src/index.js
+++ 5webpack/src/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: TerryMin
+ * @Date: 2021-03-12 14:46:49
+ * @LastEditors: TerryMin
+ * @LastEditTime: 2021-03-12 15:05:31
+ * @Description: file not
+ */
 import _ from 'lodash';
 import './style.css';
 import printMe from './print.js';
@@ -6,12 +13,13 @@ if (process.env.NODE_ENV !== 'production') {
   console.log(process);
   console.log('Looks like we are in development mode3!');
 }
-function component() {
+function component () {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.innerHTML = _.join(['Hello', 'webpack test'], ' ');
+  element.classList.add('hello');
+  
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
