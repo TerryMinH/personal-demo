1e4706cdb37efa271c44aa0cbe0d70696ca01fcf
TerryMin <15896938825@163.com>
2021-03-17 13:31:37 +0800

    vue-router

--- antd-demo/src/App.vue
+++ antd-demo/src/App.vue
@@ -2,25 +2,52 @@
  * @Author: TerryMin
  * @Date: 2021-01-07 10:44:14
  * @LastEditors: TerryMin
- * @LastEditTime: 2021-01-07 10:52:31
+ * @LastEditTime: 2021-03-15 11:09:00
  * @Description: file not
 -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
     <a-button type="primary">Button></a-button>
 
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <HelloDemo
+      v-bind:message="message"
+      v-on:increment="increamentTotal"
+    />
+    <div>{{result}}</div>
+    <div>total:{{total}}</div>
+    <div v-if="seen">现在你看到我了</div>
+    <!-- <button v-on:click="reverseMessage">反转消息</button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloDemo from './components/HelloDemo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloDemo
+  },
+  data: function () {
+    return {
+      result: '',
+      message: '页面加载于 ' + new Date().toLocaleString(),
+      seen: true,
+      total:0
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.result = '结果'
+    },
+    increamentTotal: function () {
+      this.total += 5;
+    }
   }
 }
 </script>
